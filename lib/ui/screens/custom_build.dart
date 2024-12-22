b9adaa5df572b221c6fd042b6ce279d036d80c88
Irtaza Khalid <irtazakhalidll@gmail.com>
2024-12-22 11:06:43 +0500

    Creating database

---
+++ lib/ui/screens/custom_build.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomBuild extends StatefulWidget {
+  const CustomBuild({super.key});
+
+  @override
+  State<CustomBuild> createState() => _CustomBuildState();
+}
+
+class _CustomBuildState extends State<CustomBuild> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
